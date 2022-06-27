10fed5a1707bb72aa4bd698ba300c6ef3691a3bd
AndrewYao <azusa5526@gmail.com>
2022-06-27 11:25:06 +0800

    feat: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,32 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-}
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ['plugin:vue/essential', '@vue/standard', 'prettier', 'plugin:prettier/recommended'],
+	plugins: ['prettier'],
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'prettier/prettier': 'error',
+		'arrow-body-style': 'off',
+		'prefer-arrow-callback': 'off',
+		'vue/multi-word-component-names': [
+			0,
+			{
+				ignores: ['default']
+			}
+		]
+	},
+	overrides: [
+		{
+			files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+			env: {
+				jest: true
+			}
+		}
+	]
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	printWidth: 140,
+	tabWidth: 2,
+	useTabs: true,
+	singleQuote: true,
+	semi: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	endOfLine: 'auto'
+};
